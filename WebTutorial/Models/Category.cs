2b3549057f0b0be816550ee9e82a0cd646972843
Karanvir99 <karanvirchoda4@hotmail.co.uk>
2021-06-21 17:54:54 +0100

    Add project files.

---
+++ WebTutorial/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTutorial.Models
+{
+    public class Category
+    {
+        //tell the db/entity framework core that this needs to be the primary key and needs to be identity
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Name")]
+        [Required]
+        public string CategoryName { get; set; }
+
+        [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage = "Display order for category must be greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ WebTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
